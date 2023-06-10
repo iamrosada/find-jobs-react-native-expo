e327673d09084b3cc67b326f0c4d02fa1016c4e3
iamrosada <luisrosada2018@gmail.com>
2023-06-10 17:04:57 +0300

    feat: starting the welcome componet

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from './welcome.style'
+
+import {icons, SIZES} from '../../../constants'
+
+const Welcome = () => {
+  const router = useRouter()
+ 
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text>
+          Hello Rosada
+        </Text>
+
+      </View>
+  
+    </View>
+  );
+};
+
+export default Welcome;
