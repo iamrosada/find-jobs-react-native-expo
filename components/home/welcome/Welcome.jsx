7ddb7668d0a1a8029e1a3050550ae08e894ffddb
iamrosada <luisrosada2018@gmail.com>
2023-06-10 17:34:49 +0300

    feat: created the search bar to find the job

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -9,23 +9,35 @@ import {
 } from "react-native";
 import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
 
-import {icons, SIZES} from '../../../constants'
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
-  const router = useRouter()
- 
+  const router = useRouter();
 
   return (
     <View>
       <View style={styles.container}>
-        <Text>
-          Hello Rosada
+        <Text style={styles.userName}>Hello Rosada</Text>
+        <Text style={styles.welcomeMessage}>
+          Find somebody to fix your stuff
         </Text>
-
+        <View style={styles.searchContainer}>
+          <View style={styles.searchWrapper}>
+            <TextInput 
+            style={styles.searchInput} 
+             value=""
+             onChange={()=>{}}
+             placeholder="What are you looking for?"
+             
+             />
+          </View>
+          <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}> 
+            <Image source={icons.search} resizeMede="contain" style={styles.searchBtnImage}/>
+          </TouchableOpacity>
+        </View>
       </View>
-  
     </View>
   );
 };
